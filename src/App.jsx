91eb30359ffd7cb0dd3a6239775704638da6edc6
jsheerin66 <justinsheerin@gmail.com>
2017-08-01 01:15:16 -0400

    Second Commit_UpToES2015_pg30

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
-var isContentNode = document.getElementById('contents');
-// below is first use of jsx- not a string, gets transpiled into jsx because of line 10 which is the babel cdn
-var component = <h1>Hola World!</h1>;
-// render the component inside content node, in this case the component is within content node, which is the div with the id contents
-ReactDOM.render(component, isContentNode);
+var contentNode = document.getElementById('contents');
+var component = <h1>Street Engineers_Local Streets!</h1>;        // A simple component, written in JSX
+
+ReactDOM.render(component, contentNode);      // Render the component inside the content Node
--- static/App.js
+++ static/App.js
@@ -1,9 +1,8 @@
-var isContentNode = document.getElementById('contents');
-// below is first use of jsx- not a string, gets transpiled into jsx because of line 10 which is the babel cdn
+var contentNode = document.getElementById('contents');
 var component = React.createElement(
   'h1',
   null,
-  'Hola World!'
-);
-// render the component inside content node, in this case the component is within content node, which is the div with the id contents
-ReactDOM.render(component, isContentNode);
+  'Street Engineers_Local Streets!'
+); // A simple component, written in JSX
+
+ReactDOM.render(component, contentNode); // Render the component inside the content Node
