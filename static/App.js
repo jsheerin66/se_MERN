2a5d94518ae602ac1bdb67a5ad5f3da88b08a48f
jsheerin66 <justinsheerin@gmail.com>
2017-08-03 01:12:40 -0400

    Third Commit_finished ES6 onto chapter 3

--- static/App.js
+++ static/App.js
@@ -1,8 +1,16 @@
+'use strict';
+
 var contentNode = document.getElementById('contents');
+
+var continents = ['Africa', 'America', 'Asia', 'Australia', 'Europe'];
+var message = continents.map(function (c) {
+  return 'Hello ' + c + '!';
+}).join(' ');
+
 var component = React.createElement(
-  'h1',
+  'p',
   null,
-  'Street Engineers_Local Streets!'
+  message
 ); // A simple component, written in JSX
 
 ReactDOM.render(component, contentNode); // Render the component inside the content Node
