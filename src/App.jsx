9fe301c834d5549181a97b410207cc1c31c7650b
jsheerin66 <justinsheerin@gmail.com>
2017-08-03 01:26:44 -0400

    Fourth Commit_Passing Data to child compenents_pg43

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,66 @@
 const contentNode = document.getElementById('contents');
 
-const continents = ['Africa','America','Asia','Australia','Europe'];
-const message = continents.map(c => `Hello ${c}!`).join(' ');
+class IssueFilter extends React.Component {
+  render() {
+    return (
+      <div>This is a placeholder for the Issue Filter.</div>
+    )
+  }
+}
 
-const component = <p>{message}</p>;           // A simple component, written in JSX
+class IssueRow extends React.Component {
+  render() {
+    const borderedStyle = {border: "1px solid silver", padding: 4};
+    return (
+      <tr>
+        <td style={borderedStyle}>{this.props.issue_id}</td>
+        <td style={borderedStyle}>{this.props.children}</td>
+      </tr>
+    )
+  }
+}
 
-ReactDOM.render(component, contentNode);      // Render the component inside the content Node
+class IssueTable extends React.Component {
+  render() {
+    const borderedStyle = {border: "1px solid silver", padding: 6};
+    return (
+      <table style={{borderCollapse: "collapse"}}>
+        <thead>
+          <tr>
+            <th style={borderedStyle}>Id</th>
+            <th style={borderedStyle}>Title</th>
+          </tr>
+        </thead>
+        <tbody>
+          <IssueRow issue_id={1}>Error in console when clicking Add</IssueRow>
+          <IssueRow issue_id={2}>Missing bottom <b>border</b> on panel</IssueRow>
+        </tbody>
+      </table>
+    )
+  }
+}
+
+class IssueAdd extends React.Component {
+  render() {
+    return (
+      <div>This is a placeholder for an Issue Add entry form.</div>
+    )
+  }
+}
+
+class IssueList extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Issue Tracker</h1>
+        <IssueFilter />
+        <hr />
+        <IssueTable />
+        <hr />
+        <IssueAdd />
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<IssueList />, contentNode);    // Render the component inside the content Node
